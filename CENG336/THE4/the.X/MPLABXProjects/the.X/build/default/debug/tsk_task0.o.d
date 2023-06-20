ded696bec813dde07fcf828f4626c2bd40711e8a
taylanisleyici <taylan.isleyici@gmail.com>
2023-06-21 01:35:45 +0300

    Cengizin kod

---
+++ CENG336/THE4/the.X/MPLABXProjects/the.X/build/default/debug/tsk_task0.o.d
@@ -0,0 +1,9 @@
+build/default/debug/tsk_task0.o:  \
+	/home/ceng33620232/MPLABXProjects/the.X/tsk_task0.c  \
+	/home/ceng33620232/Desktop/picos/Include/pro_man.h  \
+	/home/ceng33620232/Desktop/picos/Include/even_man.h  \
+	/home/ceng33620232/Desktop/picos/Include/alarm.h  \
+	/home/ceng33620232/MPLABXProjects/the.X/common.h  \
+	/home/ceng33620232/Desktop/picos/Include/device.h  \
+	/home/ceng33620232/Desktop/picos/Include/int_man.h  \
+
--- CENG336/THE4/the.X/MPLABXProjects/the.X/nbproject/Package-default.bash
+++ CENG336/THE4/the.X/MPLABXProjects/the.X/nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/THE4.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=THE4.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=the4.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/the.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=the.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=the.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/the4.x/bin
+makeDirectory ${TMPDIR}/the.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/the4.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/the.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/the4.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/the.x.tar *
 checkReturnCode
 
 # Cleanup
